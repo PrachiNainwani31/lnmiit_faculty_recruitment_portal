bb49ed952d75d28323b2c6c49ee37f572c78f8b5
PrachiNainwani31 <prachinainwnai31@gmail.com>
2026-04-18 02:57:01 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,14 @@ async function startServer() {
     await db.sequelize.sync();
     console.log("Tables Synced");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   } catch (error) {
-    console.error("❌ DB Connection Error:", error);
+    console.error("DB Connection Error:", error);
   }
 }
 
